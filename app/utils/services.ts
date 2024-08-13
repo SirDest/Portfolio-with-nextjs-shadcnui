30820225542282a3aa135e4c0617e08659a5eb42
Akinro Destined <104671422+SirDest@users.noreply.github.com>
2024-08-13 09:01:15 +0100

    commit

---
+++ app/utils/services.ts
@@ -0,0 +1,62 @@
+export const services = [
+  {
+    num: "01",
+    title: "Responsive Web Design",
+    description:
+      "Creating mobile-first, responsive websites that adapt seamlessly to different screen sizes.",
+  },
+  {
+    num: "02",
+    title: "Single Page Applications (SPAs)",
+    description:
+      "Building fast and interactive SPAs using modern frameworks like React or Vue.js.",
+  },
+  {
+    num: "03",
+    title: "Frontend Performance Optimization",
+    description:
+      "Optimizing websites for speed, ensuring fast load times and a smooth user experience.",
+  },
+  {
+    num: "04",
+    title: "UI/UX Design Implementation",
+    description:
+      "Translating design mockups into pixel-perfect, functional web interfaces.",
+  },
+  {
+    num: "05",
+    title: "Cross-Browser Compatibility",
+    description:
+      "Ensuring that websites function consistently across various browsers and devices.",
+  },
+  {
+    num: "06",
+    title: "API Integration",
+    description:
+      "Integrating third-party APIs and services, enabling dynamic content and functionality on the front end.",
+  },
+  {
+    num: "07",
+    title: "Web Animations",
+    description:
+      "Creating smooth, interactive animations using CSS, JavaScript, or libraries like GSAP.",
+  },
+  {
+    num: "08",
+    title: "E-commerce Solutions",
+    description:
+      "Developing and optimizing e-commerce frontends, including cart functionality, product displays, and payment gateways.",
+  },
+  {
+    num: "09",
+    title: "Accessibility",
+    description:
+      "Building accessible websites that comply with WCAG guidelines, ensuring usability for everyone.",
+  },
+  {
+    num: "10",
+    title: "Version Control and Collaboration",
+    description:
+      "Proficiency with tools like Git and GitHub for version control and collaboration with other developers.",
+  },
+];
